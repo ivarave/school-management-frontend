c9762d965ad41eea06e777eb2d50227c000c4396
Ivarave API <eivarave404@gmail.com>
2025-07-23 13:33:50 +0100

    asa

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: './',
   server: {
     historyApiFallback: true,
-  },
+  }
 });
