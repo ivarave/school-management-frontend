6b472d2f0058db9e9ba980b68b1b142a48d1c174
Ivarave API <eivarave404@gmail.com>
2025-07-23 13:55:46 +0100

    dsnd

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '.', // put inside dist/
+        }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
   },
-  base: '/', // <-- very important for static hosting
-});
+  base: '/',
+})
